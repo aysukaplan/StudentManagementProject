85a203599b2edbb43f6998c08fbc60b3ca05ed19
aysu <aysukaplan22@gmail.com>
2022-07-07 01:59:43 +0300

    6-2

---
+++ StudentProject/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StudentProject.Models;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string Email  { get; set; }
+    public string Birthday { get; set; }
+    public string GraduationDate { get; set; }
+
+}
