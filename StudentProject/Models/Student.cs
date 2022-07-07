13d872a1bef4c748fb1d006d7340f85864dddfc9
aysu <aysukaplan22@gmail.com>
2022-07-08 01:17:55 +0300

    7-1 student context and controller added

--- StudentProject/Models/Student.cs
+++ StudentProject/Models/Student.cs
@@ -3,11 +3,21 @@ namespace StudentProject.Models;
 
 public class Student
 {
-    
+    public Student(string name, int id, string email, string birthday, string graduationDate)
+    {
+        Name = name;
+        Id = id;
+        Email = email;
+        Birthday = birthday;
+        GraduationDate = graduationDate;
+    }
 
     public string Name { get; set; }
     public int Id { get; set; }
     public string Email  { get; set; }
     public string Birthday { get; set; }
     public string GraduationDate { get; set; }
+
+    public DateTime CreateDate;
+    public DateTime UpdateDate;
 }
---
+++ StudentProject/Models/StudentContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Common;
+using System.IO;
+
+namespace StudentProject.Models;
+public class StudentContext
+{
+    // reads lines from text and adds the lines string array
+
+    private readonly string[] _lines = File.ReadAllLines("students.txt"); 
+    private bool _firstList = true;
+    public List<Student> StudentList = new List<Student>();
+
+   
+    public void LinesToStudentList(){
+        foreach (var line in _lines)
+        {
+            string[] lineSplit = line.Split(",");
+            var Student = new Student(lineSplit[0],int.Parse(lineSplit[1]),lineSplit[2],lineSplit[3],lineSplit[4]);
+            StudentList.Add(Student);
+        }
+    }
+    
+    public List<Student>GetStudents(){
+        if(_firstList){
+            LinesToStudentList();
+            _firstList = false;
+        }
+        return StudentList;
+    }
+    public void SetStudents(List<Student> students){
+        StudentList = students;
+    }
+    }
