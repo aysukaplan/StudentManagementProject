19b4def9c339acda0a7d3aba1c5c9088cf39dbfe
aysu <aysukaplan22@gmail.com>
2022-07-18 04:52:25 +0300

    16,17-1

--- StudentProject/Controllers/AdminController.cs
+++ StudentProject/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using StudentProject.Models;
+using StudentProject.FileOperations;
 
 namespace StudentProject.Controllers
 {
@@ -9,102 +10,173 @@ namespace StudentProject.Controllers
 
     public class AdminController : ControllerBase
     {
-        /*
+         private readonly AdminFileOperations _adminContext;
+         private readonly StudentFileOperations _studentContext;
+
+        private readonly TeacherFileOperations _teacherContext;
+
+
+        public AdminController(AdminFileOperations AdminContext,StudentFileOperations StudentContext ,TeacherFileOperations TeacherContext)
+        {
+            _adminContext = AdminContext;
+            _studentContext = StudentContext;
+            _teacherContext = TeacherContext;
+        }
+
+        //get all admins
         [HttpGet()]
         public List<Admin> GetAdmins()
         {
-            return;
-
+            List<Admin> admins = _adminContext.GetAdminList();
+            return admins;
+    
         }
 
         [HttpGet("{id}")]
         public Admin GetAdmin(int id)
         {
-            return;
+            return _adminContext.GetAdminList().Where(x => x.Id == id).FirstOrDefault();
         }
+    
 
-        /*
-        [HttpPost] //bu yanlış oldu gibi diğerlerine de route yazmalıyım
+    
+        [HttpPost] 
 
         public IActionResult AddAdmin([FromBody] Admin newAdmin)
         {
             //Check if the admin exists
+             List<Admin> admins = _adminContext.GetAdminList();
+             var admin = admins.SingleOrDefault(x=>x.Id==newAdmin.Id);
             //If there is a admin with same name
-            return BadRequest();
+            if(admin is null)
+                return BadRequest();
 
             //If there is not a admin with same name 
-            //add admin to admin list
+            //add admin to admin txt
+            _adminContext.Write(admin.ToString());
             return Ok();
         }
 
         public IActionResult AddStudent([FromBody] Student newStudent)
         {
-            //Check if the student exists
-            //If there is a student with same name
-            return BadRequest();
+            List<Student> students = _studentContext.GetStudentList();
+             var student = students.SingleOrDefault(x=>x.Id==newStudent.Id);
+            
+            if(student is null)
+                return BadRequest();
 
-            //If there is not a student with same name 
-            //add student to student list
+            _studentContext.Write(student.ToString());
             return Ok();
         }
+        
         public IActionResult AddTeacher([FromBody] Teacher newTeacher)
         {
-            //Check if the teacher exists
-            //If there is a teacher with same name
-            return BadRequest();
+            List<Teacher> teachers = _teacherContext.GetTeacherList();
+            var teacher = teachers.SingleOrDefault(x=>x.Id==newTeacher.Id);
+         
+            if(teacher is null)
+                return BadRequest();
 
-            //If there is not a teacher with same name 
-            //add teacher to teacher list
+            _teacherContext.Write(teacher.ToString());
             return Ok();
         }
 
         //put methods to here
 
 
-        [HttpDelete("{adminId}")]
+        //[HttpDelete] //delete all students admins/{adminId}/students
+        [HttpDelete("Students")]
+        public IActionResult DeleteStudents()
+        {
+            try
+            {
+                _studentContext.Clear();
+            }
+            catch (System.Exception ex)
+            {
+                 return BadRequest();
+                 throw ex;
+            }
+            return Ok();
+        }
 
-        //deleting an admin
+
+        //[HttpDelete] //delete all teachers  admins/{adminId}/teachers
+
+        [HttpDelete("Teachers")]
+        public IActionResult DeleteTeachers()
+        {
+            try
+            {
+                _teacherContext.Clear();
+            }
+            catch (System.Exception ex)
+            {
+                 return BadRequest();
+                 throw ex;
+            }
+            return Ok();
+        }
+        [HttpDelete("{adminId}")] // admins/{adminId}
         public IActionResult DeleteAdmin(int adminId)
         {
             //check if the admin is in the admin list
             //if not send bad request 
-            return BadRequest();
-
-            //if admin with given id is in admin list
-            //check if there is another admin in list besides given admin
-            //if there is not other admin 
-            return BadRequest(); //or send another error code
-            //else
+            List<Admin> admins = _adminContext.GetAdminList();
+            var admin = admins.SingleOrDefault(x=>x.Id==adminId);
+              //if there is not other admin 
+            if(admin is null || (admins.Count ==1))
+                return BadRequest();
+           
+            _adminContext.DeleteLine(admin.ToString());//file oper. could throw ex.
             return Ok();
         }
 
-        [HttpDelete("{studentId}")]
+        [HttpDelete("{studentId}")] //admins/{adminId}/students/{studentId}
 
         //deleting a student
         public IActionResult DeleteStudent(int studentId)
         {
-            //check if the student is in the student list
-            //if not send bad request 
-            return BadRequest();
-
-            //if student with given id is in student list
+            
+            List<Student> students = _studentContext.GetStudentList();
+            var student = students.SingleOrDefault(x=>x.Id==studentId);
+            
+            if(student is null)
+                return BadRequest();
+            try
+            {
+                _studentContext.DeleteLine(student.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                  return BadRequest();
+                  throw ex;
+            }
             return Ok();
         }
-        [HttpDelete("{teacherId}")]
+
+        
+        [HttpDelete("{teacherId}")]  //admins/{adminId}/teachers/{teacherId}
 
         //deleting a teacher
         public IActionResult DeleteTeacher(int teacherId)
         {
-            //check if the teacher is in the teacher list
-            //if not send bad request 
-            return BadRequest();
-
-            //if teacher with given id is in teacher list
+            List<Teacher> teachers = _teacherContext.GetTeacherList();
+            var teacher = teachers.SingleOrDefault(x=>x.Id==teacherId);
+            
+            if(teacher is null)
+                return BadRequest();
+            try
+            {
+                _teacherContext.DeleteLine(teacher.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                  return BadRequest();
+                  throw ex;
+            }
             return Ok();
         }
-
-        //[HttpDelete] //delete all students?
-        //[HttpDelete] //delete all teachers?
-        */
+        
     }
 }
--- StudentProject/FileOperations/StudentFileOperations.cs
+++ StudentProject/FileOperations/StudentFileOperations.cs
@@ -4,27 +4,74 @@ using System.IO;
 
 namespace StudentProject.FileOperations;
 
-public class StudentFileOperations : FileOperations
+public class StudentFileOperations : IFileOperations
 {
-    public readonly string  FileName = "student.txt";
+    private string _filePath = "Datas/student.txt";
 
-    public override void Read()
+
+    public StudentFileOperations()
+    {
+    }
+    public void Read()
     {
 
     }
 
-    public override void Write()
+    public void Write(string line)
     {
-
+        StreamWriter writer = new StreamWriter(_filePath);
+        writer.WriteLine(line);
+        writer.Close();
     }
 
-    public override void Clear()
+    public void Clear()
     {
-
+        FileStream fileStream = File.Open(_filePath, FileMode.Open);
+        fileStream.SetLength(0);
+        fileStream.Close();
     }
+    public void DeleteLine(string line){
+        // 1. Read the content of the file
+        string[] readText = File.ReadAllLines(_filePath);
 
-    public override List<Person>  GetList()
+        // 2. Empty the file
+        File.WriteAllText(_filePath, String.Empty);
+
+        // 3. Fill up again, but without the deleted line
+        using (StreamWriter writer = new StreamWriter(_filePath))
+        {
+            foreach (string s in readText)
+            {
+                if (!s.Equals(line))
+                {
+                    writer.WriteLine(s);
+                }
+            }
+        }
+    }
+    
+    public List<Student> GetStudentList()
     {
-       return new List<Person>();
+        //eğer dosya boştaysa??
+        List<Student> StudentList = new List<Student>();
+        // reads lines from text and adds the lines string array
+        string[] _lines = {};
+        try
+        {
+            _lines = File.ReadAllLines(_filePath);
+
+        }
+        catch (Exception e)
+        {
+            throw  e;
+        }
+        foreach (var line in _lines)
+        {
+            string[] lineSplit = line.Split(",");
+            var student = new Student(lineSplit[0],int.Parse(lineSplit[1]),lineSplit[2],
+                lineSplit[3],lineSplit[4],int.Parse(lineSplit[5]));
+            StudentList.Add(student);
+        }
+        return StudentList;
     }
 }
